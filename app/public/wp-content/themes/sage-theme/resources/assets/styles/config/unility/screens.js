200f5f4f17c457d1f439b02cd058f419346ec92a
PhuongNam12345 <91358362+PhuongNam12345@users.noreply.github.com>
2022-12-01 12:00:09 +0700

    12/1

--- app/public/wp-content/themes/sage-theme/resources/assets/styles/config/unility/screens.js
+++ app/public/wp-content/themes/sage-theme/resources/assets/styles/config/unility/screens.js
@@ -1,9 +1,9 @@
 const Screens = {
   'sm': '481px',
   'md': '768px',
-  'lg': '992px',
-  'xl': '1200px',
-  '2xl': '1440px',
+  'lg': '1024px',
+  'xl': '1440px',
+  '2xl': '1920px',
   '2k': '2000px',
   'down_xl': {'max': '1199px'},
   'down_lg': {'max': '991px'},
--- app/public/wp-content/themes/sage-theme/resources/views/template-parts/page/module-mod_twocol.blade.php
+++ app/public/wp-content/themes/sage-theme/resources/views/template-parts/page/module-mod_twocol.blade.php
@@ -1,38 +1,40 @@
 
-    <section class="flex justify-center w-full  bg-primary ">
-        <div class="w-2/5">
-            <div class="Line-4 bg-primary border-8"></div>
-            <span class="The-Encompass text-left ">
-                The Encompass Platform Seamlessly Connects the Supply Chain, Allowing You to:
-              </span>
-        </div>
-        <div class="flex w-3/5">
-            <div class="column1 text-left">
-                <div class="row1">
-                    <span class="Drive-Business text-left ">
-                        Drive Business Growth
-                    </span>
-                    <p>Tap into the power of predictive analytics and demand forecasting. Capitalize on revenue-generating opportunities faster.  Capture mission-critical data across your entire operation—including all three supply chain tiers—empowering you to bring your beverage or food business into new levels of profitability. </p>
-                </div>
-                <div class="row2">
-                    <span class="Improve-Operational text-left ">
-                        Improve Operational Efficiencies
-                      </span>
-                      <p>Treamline processes and save money by reducing leakage with a fully integrated ERP system that works for your business. Encompass ERP makes it possible to identify where, when, and how resources can be best allocated to provide the most optimal outcome.</p>                   
-                </div>
+    <section class=" flex justify-center w-full  ">
+        <div class=" w-4/5 flex flex-col xl:w-4/5 xl:flex-row ">
+            <div class="w-full lg:mb-[51px] lg:mr-[102px] xl:mr-[87px] xl:flex flex-col xl:w-2/5">
+                <div class="Line-4 mb-[9px]"></div>
+                <h1 class="content text-left text-[36px] ">
+                    The Encompass Platform Seamlessly Connects the Supply Chain, Allowing You to:
+                </h1>
             </div>
-            <div class="text-left ">
-                <div class="row1">
-                    <span class="Leverage-Real-Time text-left ">
-                        Leverage Real-Time Data for Better Decision-Making
-                    </span>
-                    <p>Accelerate business decisions with better, more accurate system-to-system data and comprehensive reporting that results in deep, actionable insights. Stay ahead of potential disruptions and maintain a finger on the pulse of your supply chain from anywhere in the world.</p>
+            <div class="flex-col flex w-full lg:flex-row xl:w-full ">
+                <div class=" text-left  lg:mr-[55px] ">
+                    <div class="lg:mb-[55px]">
+                        <h3 class="content text-left text-[24px]  ">
+                            Drive Business Growth
+                        </h3>
+                        <p class="text">Tap into the power of predictive analytics and demand forecasting. Capitalize on revenue-generating opportunities faster.  Capture mission-critical data across your entire operation—including all three supply chain tiers—empowering you to bring your beverage or food business into new levels of profitability. </p>
+                    </div>
+                    <div class="">
+                        <h3 class="content text-left text-[24px] ">
+                            Improve Operational Efficiencies
+                        </h3>
+                        <p class="text">Treamline processes and save money by reducing leakage with a fully integrated ERP system that works for your business. Encompass ERP makes it possible to identify where, when, and how resources can be best allocated to provide the most optimal outcome.</p>                   
+                    </div>
                 </div>
-                <div class="row2">
-                    <span class="Enhance-Team-Dynamics text-left ">
-                        Enhance Team Dynamics & Employee Satisfaction
-                      </span>
-                    <p>  When your labor force has the tools they need to succeed, the workplace runs like a well-oiled machine. With Encompass ERP, every stakeholder across your supply chain—from purchasing to Point-of-Sale—has what they need to do their jobs effectively. </p>
+                <div class="text-left ">
+                    <div class="row1">
+                        <h3 class="content  text-left  text-[24px] ">
+                            Leverage Real-Time Data for Better Decision-Making
+                        </h3>
+                        <p class="text">Accelerate business decisions with better, more accurate system-to-system data and comprehensive reporting that results in deep, actionable insights. Stay ahead of potential disruptions and maintain a finger on the pulse of your supply chain from anywhere in the world.</p>
+                    </div>
+                    <div class="">
+                        <h3 class="content text-left text-[24px] ">
+                            Enhance Team Dynamics & Employee Satisfaction
+                        </h3>
+                        <p class="text">  When your labor force has the tools they need to succeed, the workplace runs like a well-oiled machine. With Encompass ERP, every stakeholder across your supply chain—from purchasing to Point-of-Sale—has what they need to do their jobs effectively. </p>
+                    </div>
                 </div>
             </div>
         </div>
